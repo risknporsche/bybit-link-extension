e78c75f466a28101cfd2a46588aa32037be0914c
risknporsche <risknporsche@gmail.com>
2025-12-10 17:49:45 +0000

    Fix Get KYC Link

--- src/api/kyc.ts
+++ src/api/kyc.ts
@@ -94,3 +94,21 @@ export const getKysInfo = async (payload: GetKysInfoPayload) => {
       throw new Error(`Error get KYC info: ${JSON.stringify(data)}`);
     });
 }
+
+export const postAmlKycQuestionnaire = (templateCode: any, content: string)=> {
+  return axios
+    .post<BybitApiResp<any>>(`/x-api/v3/private/kyc/submit-questionnaire`, {
+      biz_from: 'kyc_web',
+      template_code: templateCode,
+      content: content,
+    })
+    .then(({ data }) => {
+      if (data.ret_code === 0 || data.ret_code === 1032) {
+        return data;
+      }
+
+      throw new Error(
+        `Error post AML Kyc Questionnaire: ${JSON.stringify(data)}`
+      );
+    });
+}
